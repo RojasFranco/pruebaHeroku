41dcb154df51c4c7b6ac60f804221ef8452526bf
RojasFranco <rojas.franco.93@gmail.com>
2020-08-27 22:27:03 -0300

    primer ejercicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'primer-proyecto';
+  public edadUno: string;
+  public edadDos: string;
+  public promedio: number;
+  public suma: number;
+
+  public realizarCalculos(){        
+    this.suma = parseInt(this.edadUno) + parseInt(this.edadDos);
+    this.promedio = (this.suma)/2;
+  }
+
+  public limpiarCuadros(){
+    this.edadUno = null;
+    this.edadDos = null;
+    this.suma = null;
+    this.promedio = null;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NuevoComponenteComponent } from './componentes/nuevo-componente/nuevo-componente.component';
+import { FormsModule } from '@angular/forms';
+import { BienvenidoComponent } from './componentes/bienvenido/bienvenido.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { ErrorComponent } from './componentes/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NuevoComponenteComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
